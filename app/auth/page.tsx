544a739dbc7a9aaaa45b605bf645f48634e7ebd0
abldxtr <110703641+abldxtr@users.noreply.github.com>
2024-12-19 05:51:54 +0000

    Initial commit

---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+// import { AuthScreen } from "@/features/auth/components/auth-screen";
+
+import { AuthScreen } from "@/components/auth/auth-screen";
+
+const AuthPage = () => {
+  return <AuthScreen />;
+};
+
+export default AuthPage;
